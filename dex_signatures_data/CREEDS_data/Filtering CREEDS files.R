964773fe112c21d7a4d535dd8b834530ca48c1b6
Clara Chen <clarachen@claras-air-3.mynetworksettings.com>
2022-06-27 11:19:49 -0400

    Filtering CREEDS files

--- dex_signatures_data/CREEDS_data/Filtering CREEDS files.R
+++ dex_signatures_data/CREEDS_data/Filtering CREEDS files.R
@@ -7,7 +7,21 @@ DM <- read_csv("single_drug_perturbations-DM.csv")
 p1 <- read_csv("single_drug_perturbations-p1.0.csv")
 v1 <- read_csv("single_drug_perturbations-v1.0.csv")
 
-# filtering for dex
-DM %>% filter(drug_name == "Dexamethasone")
-p1 %>% filter(row_number() %in% grep("dexamethasone", p1$drug_name))
+# filtering for dex and related drugs
+DM_filtered <- 
+DM %>% filter(drug_name == "Dexamethasone"
+              | drug_name == "Betamethasone"
+              | drug_name == "Prednisolone" 
+              | drug_name == "Ketorolac")
+
+p1_filtered <- 
+p1 %>% filter(row_number() %in% c(grep("dexamethasone", p1$drug_name), 
+                                  grep("prednisolone", p1$drug_name)))
+
+v1_filtered <-
 v1 %>% filter(drug_name == "Dexamethasone")
+
+# saving files
+write_csv(DM_filtered, "filtered_drug_perturbations-DM.csv")
+write_csv(p1_filtered, "filtered_drug_perturbations-p1.0.csv")
+write_csv(v1_filtered, "filtered_drug_perturbations-v1.0.csv")
